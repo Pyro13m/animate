f855d24bc5ca78bd27aad9899dc6401917372931
Kirti0808 <gupta.kirti0808@gmail.com>
2020-05-02 23:04:55 +0530

    static to staticfiles

--- animate/settings.py
+++ animate/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
-STATIC_DIR = os.path.join(BASE_DIR,"static")
+STATIC_DIR = os.path.join(BASE_DIR,"staticfiles")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -137,9 +137,9 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = [
-    STATIC_DIR
+    STATIC_ROOT
     ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
