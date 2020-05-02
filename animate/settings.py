8afde9ded8248cd1481f3f7a023f18fb320529b2
Kirti0808 <gupta.kirti0808@gmail.com>
2020-05-02 22:31:39 +0530

    Initial deployment

--- animate/settings.py
+++ animate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,9 +135,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR
     ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+django_heroku.settings(locals())
